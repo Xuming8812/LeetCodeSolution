37cba0e2bc9da2e7217652daa70f3dee5e88af8b
Xuming8812 <ming_xu1@brown.edu>
2019-01-18 12:33:47 -0500

    Update 24

--- LeetCodeSolution/21 Merge Two Sorted Lists.cpp
+++ LeetCodeSolution/21 Merge Two Sorted Lists.cpp
@@ -8,6 +8,10 @@
 
 using namespace std;
 
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+*/
+
 struct ListNode {
 	int val;
 	ListNode *next;
---
+++ LeetCodeSolution/24 Swap Nodes in Pairs.cpp
@@ -0,0 +1,46 @@
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+/*
+Given a linked list, swap every two adjacent nodes and return its head.
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+*/
+
+ListNode* swapPairs(ListNode* head) {
+
+	if (head == nullptr || head->next == nullptr) { return head; }
+
+	ListNode *left{ head }, *right{ head->next }, *result{ head->next }, *previous{ new ListNode(INT_MAX) };
+
+	while (left != nullptr && right != nullptr)
+	{
+		previous->next = right;
+		left->next = right->next;
+		right->next = left;
+
+		if (left->next == nullptr || left->next->next == nullptr) { return result; }
+
+		previous = left;
+		right = left->next->next;
+		left = left->next;
+
+	}
+
+	return result;
+}
