333d8a9f15eb186ea61886b7cea98a3743b1ae26
Xuming8812 <ming_xu1@brown.edu>
2019-01-18 16:02:46 -0500

    Update 28 by brutal force

---
+++ LeetCodeSolution/28 Implement strStr().cpp
@@ -0,0 +1,49 @@
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+
+using namespace std;
+
+/*
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+
+//brutal force
+int strStr(string haystack, string needle) {
+	if (needle.empty()) {
+		return 0;
+	}
+
+	if (haystack.size() < needle.size()) {
+		return -1;
+	}
+
+	for (int i = 0; i < haystack.size() - needle.size() + 1; i++) {
+
+		if (haystack[i] == needle[0]) {
+			bool isFound{ true };
+
+			for (int j = 0; j < needle.size(); j++)
+			{
+				if (needle[j] != haystack[i + j])
+				{
+					isFound = false;
+					break;
+				}
+			}
+
+			if (isFound) {
+				return i;
+			}
+		}
+	}
+
+	return -1;
+}
