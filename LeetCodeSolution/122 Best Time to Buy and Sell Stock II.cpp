75d2ade469a91ad615f99b7a848c52a39036d4a6
Xuming8812 <ming_xu1@brown.edu>
2019-01-28 16:02:14 -0500

    Solve 122

---
+++ LeetCodeSolution/122 Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,31 @@
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+
+using namespace std;
+
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like 
+(i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+*/
+
+// the total profit is the sum of all the peaks substract the previous vallies
+int maxProfit(vector<int>& prices) {
+	int result{ 0 };
+
+	for (int i = 1; i < prices.size(); i++) {
+		if (prices[i] > prices[i - 1]) {
+			result += (prices[i] - prices[i - 1]);
+		}
+	}
+
+	return result;
+}
