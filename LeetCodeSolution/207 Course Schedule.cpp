10eaee1eca94b99565b7e8f902c8599b0546da59
Xuming8812 <ming_xu1@brown.edu>
2019-01-31 11:13:52 -0500

    Solve 207 by bfs

---
+++ LeetCodeSolution/207 Course Schedule.cpp
@@ -0,0 +1,55 @@
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+#include<bitset>
+using namespace std;
+
+/*
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+*/
+
+bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+	vector<set<int>> graph(numCourses);
+	vector<int> visited(numCourses);
+
+	for (auto item : prerequisites) {
+		graph[item.second].insert(item.first);
+		visited[item.first]++;
+	}
+
+	queue<int> bfs;
+
+	for (int i = 0; i < numCourses; i++) {
+		if (visited[i] == 0) {
+			bfs.push(i);
+		}
+	}
+
+	int result{ 0 };
+
+	while (!bfs.empty()) {
+		int cur = bfs.front(); bfs.pop();
+
+		result++;
+
+		for (auto item : graph[cur]) {
+
+			visited[item]--;
+
+			if (visited[item] == 0) {
+				bfs.push(item);
+			}
+
+		}
+	}
+
+	return result == numCourses;
+}
