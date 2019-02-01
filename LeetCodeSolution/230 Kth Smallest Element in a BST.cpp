596b3c31726583c75c3777d7b70832b68c062f35
Xuming8812 <ming_xu1@brown.edu>
2019-02-01 11:17:37 -0500

    Solve 230 by recursive method

---
+++ LeetCodeSolution/230 Kth Smallest Element in a BST.cpp
@@ -0,0 +1,44 @@
+﻿#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+#include<bitset>
+using namespace std;
+
+/*
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+*/
+
+ struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+ void helper(TreeNode* root, int &k, int & result) {
+	 if (k == 0 || root == nullptr) {
+		 return;
+	 }
+
+	 helper(root->left, k, result);
+	 if (k > 0) {
+		 result = root->val;
+		 k--;
+	 }
+	 helper(root->right, k, result);
+ }
+
+ int kthSmallest(TreeNode* root, int k) {
+	 int result{ 0 };
+
+	 helper(root, k, result);
+
+	 return result;
+ }
