11d73d6ed259659a13f95a53b48aa4d9a5621c9b
Xuming8812 <ming_xu1@brown.edu>
2019-02-12 21:22:47 -0500

    Solving 365 by bfs

---
+++ LeetCodeSolution/365 Water and Jug Problem.cpp
@@ -0,0 +1,52 @@
+﻿#include <vector>
+#include <map>
+#include <string>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <set>
+#include <bitset>
+
+using namespace std;
+
+/*
+Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+*/
+
+bool canMeasureWater(int x, int y, int z) {
+	if (x > y) {
+		swap(x, y);
+	}
+
+	set<int> results;
+
+	queue<int> bfs;
+
+	bfs.push(0);
+
+	while (!bfs.empty()) {
+		int current = bfs.front();
+		bfs.pop();
+
+		if (current == z) {
+			return true;
+		}
+
+		if (results.find(current) == results.end() && current >= 0) {
+			results.insert(current);
+
+			if (current <= x) {
+				bfs.push(x + current);
+				bfs.push(y + current);
+				bfs.push(y - (x - current));
+				bfs.push(x - (y - current));
+			}
+			else if (current < y) {
+				bfs.push(x + current);
+				bfs.push(x - (y - current));
+			}
+		}
+	}
+
+	return false;
+}
