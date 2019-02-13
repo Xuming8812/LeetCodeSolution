f082a535e23fa19063a205d737fc74ee179f1cff
Xuming8812 <ming_xu1@brown.edu>
2019-02-12 21:24:27 -0500

    Solve 367 by binary search

---
+++ LeetCodeSolution/367 Valid Perfect Square.cpp
@@ -0,0 +1,35 @@
+﻿#include <vector>
+#include <map>
+#include <string>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <set>
+#include <bitset>
+
+using namespace std;
+
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+*/
+
+bool isPerfectSquare(int num) {
+	if (num < 0) return false;
+
+	long start = 0, end = num;
+
+	while (start < end) {
+
+		long mid = start + (end - start) / 2;
+
+		if (mid*mid >= num) {
+			end = mid;
+		}
+		else {
+			start = mid + 1;
+		}
+	}
+
+	return start * start == num;
+
+}
