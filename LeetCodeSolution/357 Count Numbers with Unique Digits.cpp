5b5ba4e766a1a5ff21c882e84ce698055c055134
Xuming8812 <ming_xu1@brown.edu>
2019-02-12 21:10:21 -0500

    Solve 357 by backtracking

---
+++ LeetCodeSolution/357 Count Numbers with Unique Digits.cpp
@@ -0,0 +1,41 @@
+﻿#include <vector>
+#include <map>
+#include <string>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <set>
+#include <bitset>
+
+using namespace std;
+
+/*
+Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+*/
+
+int countNumbersWithUniqueDigits(int n) {
+	vector<bool> used(10, false);
+
+	return helper(n, used, 0);
+
+}
+
+int helper(int n, vector<bool>& used, int digits) {
+	if (digits == n) {
+		return 1;
+	}
+
+	int result{ 1 };
+
+	for (int i = (digits == 0) ? 1 : 0; i < 10; i++) {
+		if (!used[i]) {
+			used[i] = true;
+
+			result += helper(n, used, digits + 1);
+
+			used[i] = false;
+		}
+	}
+
+	return result;
+}
