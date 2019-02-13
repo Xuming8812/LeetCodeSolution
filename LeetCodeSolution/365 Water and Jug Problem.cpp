156ceb9797ba4e005286f39e0a9b0f2d749860b1
Xuming8812 <ming_xu1@brown.edu>
2019-02-12 21:25:05 -0500

    Update the description of 365

--- LeetCodeSolution/365 Water and Jug Problem.cpp
+++ LeetCodeSolution/365 Water and Jug Problem.cpp
@@ -10,7 +10,16 @@
 using namespace std;
 
 /*
-Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+You are given two jugs with capacities x and y litres. There is an infinite amount of water supply available.
+You need to determine whether it is possible to measure exactly z litres using these two jugs.
+
+If z liters of water is measurable, you must have z liters of water contained within one or both buckets by the end.
+
+Operations allowed:
+
+Fill any of the jugs completely with water.
+Empty any of the jugs.
+Pour water from one jug into another till the other jug is completely full or the first jug itself is empty.
 */
 
 bool canMeasureWater(int x, int y, int z) {
