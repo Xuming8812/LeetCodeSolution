cb9aabbdd2771100b319609d50138a873b667bf6
Xuming8812 <ming_xu1@brown.edu>
2019-04-26 18:32:12 -0400

    add Lint807

---
+++ Amazon Tag/Easy/(Lint807)Palindrome Number II.cpp
@@ -0,0 +1,37 @@
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+
+using namespace std;
+
+/*
+Determines whether a binary representation of a non-negative integer n is a palindrome
+*/  
+
+/**
+ * @param n: non-negative integer n.
+ * @return: return whether a binary representation of a non-negative integer n is a palindrome.
+ */
+bool isPalindrome(int n) {
+    // Write your code here
+    
+    vector<int> digits;
+    while(n){
+        digits.push_back(n&1);
+        n>>=1;
+    }
+    
+    int length = digits.size();
+    
+    for(int i = 0;i<length/2;i++){
+        if(digits[i] !=digits[length-i-1]){
+            return false;
+        }
+    }
+    
+    return true;
+}
