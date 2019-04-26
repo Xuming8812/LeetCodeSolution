3f959f936394d983fed8446c554984e2b5d0fce4
Xuming8812 <ming_xu1@brown.edu>
2019-04-25 20:38:36 -0400

    leet 762

---
+++ Amazon Tag/Easy/102(Lint69)Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,61 @@
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+#include<bitset>
+
+using namespace std;
+
+/*
+Given a binary tree, find the subtree with maximum sum. Return the root of the subtree.
+*/
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+    
+    /**
+     * @param root: A Tree
+     * @return: Level order a list of lists of integer
+     */
+    vector<vector<int>> levelOrder(TreeNode * root) {
+        // write your code here
+        vector<vector<int>> result;
+        if(root == nullptr){
+            return result;
+        }
+        
+        queue<TreeNode*> bfs;
+        bfs.push(root);
+        
+        while(!bfs.empty()){
+            int nums = bfs.size();
+            vector<int> temp;
+            
+            for(int i = 0;i<nums;i++){
+                TreeNode* current = bfs.front();
+                bfs.pop();
+                
+                temp.push_back(current->val);
+                
+                if(current->left){
+                    bfs.push(current->left);
+                }
+                
+                if(current->right){
+                    bfs.push(current->right);
+                }
+                
+            }
+            
+            result.push_back(temp);
+        }
+        
+        return result;
+    }
---
+++ LeetCodeSolution/Easy/762(Lint1046)Prime Number of Set Bits in Binary Representation.cpp
@@ -0,0 +1,51 @@
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+#include<bitset>
+
+using namespace std;
+
+/*
+Given two integers L and R, find the count of numbers in the range [L, R] (inclusive) 
+having a prime number of set bits in their binary representation.
+*/
+
+
+/**
+ * @param L: an integer
+ * @param R: an integer
+ * @return: the count of numbers in the range [L, R] having a prime number of set bits in their binary representation
+ */
+int countPrimeSetBits(int L, int R) {
+    // Write your code here
+    
+    int result = 0;
+    
+    for(int i = L; i<=R;i++){
+        int num = i;
+        
+        int count = 0;
+        
+        while(num){
+            if(num%2 == 1){
+                count++;
+            }
+            
+            num = num>>1;
+        }
+        
+        if(isPrime(count)){
+            result++;
+        }
+    }
+    
+    return result;
+}
+
+bool isPrime(int num){
+    return num == 2 || num ==3 || num == 5 || num == 7 || num == 11 || num == 13 || num == 17 || num ==19;
+}
