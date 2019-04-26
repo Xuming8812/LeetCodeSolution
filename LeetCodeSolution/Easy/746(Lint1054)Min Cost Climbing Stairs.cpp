788abe5c7a1084897697beca77fdf9cacca5348f
Xuming8812 <ming_xu1@brown.edu>
2019-04-25 22:41:11 -0400

    add min cost climbing

---
+++ LeetCodeSolution/Easy/746(Lint1054)Min Cost Climbing Stairs.cpp
@@ -0,0 +1,40 @@
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+#include<bitset>
+
+using namespace std;
+
+/*
+On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+
+Once you pay the cost, you can either climb one or two steps. 
+You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+*/
+    
+    
+/**
+ * @param cost: an array
+ * @return: minimum cost to reach the top of the floor
+ */
+int minCostClimbingStairs(vector<int> &cost) {
+    // Write your code here
+    
+    if(cost.size()<2) return 0;
+    if(cost.size()==2) return min(cost[0],cost[1]);
+    
+    int length = cost.size();
+    cost.push_back(0);
+    
+    vector<int> dp = cost;
+
+    for(int index = 2;index<=length;index++){
+        dp[index] = min(dp[index-1],dp[index-2])+cost[index];
+    }
+    
+    return dp[length];
+}
