4465429c0f2702cced686060fcbde1a2ec329418
Xuming8812 <ming_xu1@brown.edu>
2019-04-26 21:36:44 -0400

    add modern ludo 1 use a vector to store connections

---
+++ Amazon Tag/Easy/(Lint1565)Modern Ludo I.cpp
@@ -0,0 +1,56 @@
+#include<vector>
+#include<map>
+#include<string>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<set>
+#include<bitset>
+
+using namespace std;
+
+/*
+There is a one-dimensional board with a starting point on the far left side of the board and an end point on the far right side of the board. 
+There are several positions on the board that are connected to other positions, ie if A is connected to B, then when chess falls at position A, 
+you can choose whether to move the chess from A to B. And the connection is one way, which means that the chess cannot move from B to A. 
+Now given the length and connections of the board, and you have a six-sided dice(1-6), output the minimum steps to reach the end point.
+*/
+
+/**
+ * @param length: the length of board
+ * @param connections: the connections of the positions
+ * @return: the minimum steps to reach the end
+ */
+int modernLudo(int length, vector<vector<int>> &connections) {
+    // Write your code here
+    if(length<=7) return 1;
+    
+    vector<int> link(length+1);
+    
+    for(int i = 0;i<=length;i++){
+        link[i] = i;
+    }
+    vector<int> dp(length+1,INT_MAX);
+    
+    for(auto item : connections){
+        link[item[0]] = link[item[1]];
+    }
+    
+    dp[1] =0;
+    
+    for(int i = 2;i<=length;i++){
+        if(i<7){
+            dp[i] = 1;
+        }
+        else{
+            for(int j = i-1;j>=i-6;j--){
+                dp[i] = min(dp[i],dp[j]+1);
+            }
+        }
+        
+        dp[link[i]] = min(dp[i],dp[link[i]]);
+    }
+    
+    return dp[length];
+
+}
